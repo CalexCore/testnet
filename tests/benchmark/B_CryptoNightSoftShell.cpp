7454f81494ec69aee75a9e5bf1a2095484556994
Michael Herwig <michael.herwig@hotmail.de>
2018-11-20 06:59:32 +0100

    [BM] basis softshell benchmark

--- tests/benchmark/B_CryptoNightSoftShell.cpp
+++ tests/benchmark/B_CryptoNightSoftShell.cpp
@@ -1,18 +1,29 @@
 ﻿#include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
+#include <random>
+#include <crypto/hash.h>
+
+using random_bytes_engine = std::independent_bits_engine<
+    std::default_random_engine, CHAR_BIT, uint16_t>;
+
+static void HeightArguments(benchmark::internal::Benchmark* b) {
+  for (int i = 0; i <= CN_SOFT_SHELL_WINDOW * 2; ++i)
+      b->Arg(i);
+}
+
+static void BM_CN_SoftShell(benchmark::State& state) {
+  random_bytes_engine rbe;
+  std::vector<uint16_t> data;
+  data.resize(76 / 2);
+  std::generate(begin(data), end(data), std::ref(rbe));
+
+  uint32_t height = static_cast<uint32_t>(state.range(0));
+
+  Crypto::Hash hash;
   for (auto _ : state)
-    std::string empty_string;
+    cn_soft_shell_slow_hash_v1(data.data(), data.size() * 2, hash, height);
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_CN_SoftShell)->Apply(HeightArguments);
 
 BENCHMARK_MAIN();
