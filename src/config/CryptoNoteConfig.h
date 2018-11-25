60b4a7dd19647760903ca965e8c2028a6d3ec2cd
Michael Herwig <michael.herwig@hotmail.de>
2018-11-25 03:28:44 +0100

    [CFG] Changed GenHex to mainnet

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -79,7 +79,7 @@ TurtleCoind --print-genesis-tx --genesis-block-reward-address TRTLv2Fyavy8CXG8BP
 * You should see your premine appear in the previously generated wallet.
 
 */
-const char     GENESIS_COINBASE_TX_HEX[]                     = "01e80201ff0001f5d771029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121016833ec4d0f9365cd9ebea1dd4674cd014c33a298c4b0622fa37fc5db692b2ac4";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "01e80201ff0001000297e09858b9cfc8c7b5920e995472172859b18a84538f256742ce215859659d3f2101e5c80d1e21bbb6308b2d09b8b4dda3c3b4f3e811dfbcb708996dbb8e91d6eccf";
 
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
    You can get this value by doing "print_block 2" in TurtleCoind. It is used to know what timestamp
